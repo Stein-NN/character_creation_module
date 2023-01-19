0579d58bca76eba5aeb03520370a0c228001c6d0
Дмитрий Елизаров <elizarovdmitrij@gmail.com>
2023-01-19 17:06:39 +0300

    code according to PEP8

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ main()
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Геймплей."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -51,6 +52,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Геймплей."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -60,6 +62,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Навыки."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -70,6 +73,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Описание игрока."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -94,6 +98,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Выбор игрока."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
